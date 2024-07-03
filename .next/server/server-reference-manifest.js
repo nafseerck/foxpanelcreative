65d91e3baebd0011f2a73221f46b1545b3bb47de
nafseerck <nafseer.ceekey@gmail.com>
2024-07-03 14:13:51 +0400

    first commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Fx/M31aEDf1mI9MxPN9LNtfqSAR/DCmQpXNPYYXFrjQ=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <>
+      <div className="container text-center pt-5 mt-5">
+        <div className="row">
+          <div className="col">
+            <h3 className="text-xl">
+              Hello PQs this is the page
+            </h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
